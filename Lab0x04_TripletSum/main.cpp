aae698204a0b01b8c451aa06b23dd621f4afca95
Nicholas Buras <nicholas.buras@trojans.dsu.edu>
2021-11-07 20:19:27 -0800

    Initial Commit

---
+++ Lab0x04_TripletSum/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab0x04_TripletSum
+//
+//  Created by Nicholas Buras on 11/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
